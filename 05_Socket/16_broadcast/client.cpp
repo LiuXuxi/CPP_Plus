df9e84c5c159b508ce2c13c61df564aa10849913
xuedu <1106630790@qq.com>
2026-04-14 15:45:02 +0800

    broadcast

---
+++ 05_Socket/16_broadcast/client.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include <sys/types.h>        
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#define SER_IP "192.168.31.255"
+#define SER_PORT 8889
+#define CLI_IP "192.168.31.129"
+#define CLI_PORT 8889
+
+int main(){
+    //1.创建套接字
+    int sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    if(sockfd==-1){
+        perror("socket error");
+        return -1;
+    }
+    printf("sockfd is %d\n",sockfd);
+
+    //设置套接字属性
+    int broadcast=1;
+    socklen_t len=sizeof(broadcast);
+    if((setsockopt(sockfd,SOL_SOCKET,SO_BROADCAST,&broadcast,len))==-1){
+        perror("setsocktopt error");
+        return -1;
+    }
+
+    
+    //3.连接服务端
+    //3.1创建并初始化服务端的通信信息结构体
+    struct sockaddr_in sin;
+    sin.sin_family=AF_INET;
+    sin.sin_port=htons(SER_PORT);
+    sin.sin_addr.s_addr=inet_addr(SER_IP);
+
+    //4.向服务端发送消息，并接受服务端回应的消息
+    char wbuf[128];
+    char rbuf[128];
+    while(1){
+        //4.0清空容器
+        memset(wbuf,0,sizeof(wbuf));
+        //4.1构造消息：从终端读取消息
+        printf("please input>>>");
+        fgets(wbuf,sizeof(wbuf),stdin);
+        wbuf[strlen(wbuf)-1]=0;
+
+        //4.2向服务端发送消息
+        sendto(sockfd,wbuf,strlen(wbuf),0,(sockaddr*)&sin,sizeof(sin));
+
+        //4.3接受服务端回应的消息
+        recvfrom(sockfd,rbuf,sizeof(rbuf),0,NULL,NULL);
+        printf("recv from [%s:%d]:%s\n",inet_ntoa(sin.sin_addr),ntohs(sin.sin_port),rbuf);
+
+        //4.4接受quit时结束通信
+        if(strcmp(wbuf,"quit")==0)break;
+    }
+    
+    //5.关闭网络通信
+    close(sockfd);
+    printf("close socket...\n");
+    return 0;
+}
---
+++ 05_Socket/16_broadcast/server.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include <sys/types.h>        
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#define SER_IP INADDR_ANY
+#define SER_PORT 8889
+
+int main(){
+    //1.创建套接字
+    int sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    if(sockfd==-1){
+        perror("socket error");
+        return -1;
+    }
+    printf("sockfd is %d\n",sockfd);
+
+    //2.绑定IP和PORT
+    //2.1创建并初始化地址信息结构体
+    struct sockaddr_in sin;
+    sin.sin_family=AF_INET;
+    sin.sin_port=htons(SER_PORT);
+    sin.sin_addr.s_addr=SER_IP;
+    
+    if((bind(sockfd,(const sockaddr*)&sin,sizeof(sin)))==-1){
+        perror("bind error");
+        return -1;
+    }
+    printf("bind succeed\n");
+
+    //4.接受一个客户端
+    //记录客户端通信信息的结构体
+    struct sockaddr_in cin;
+    socklen_t len=sizeof(cin);
+
+    //5.接受并处理客户端发送的消息
+    char rbuf[128];
+    char callback[128]="*_*";
+    while(1){
+        //清空容器
+        memset(rbuf,0,sizeof(rbuf));
+        //阻塞接受
+        recvfrom(sockfd,rbuf,sizeof(rbuf),0,(sockaddr*)&cin,&len);
+        printf("recv from [%s:%d]:%s\n",inet_ntoa(cin.sin_addr),ntohs(cin.sin_port),rbuf);
+        //给客户端回复消息
+        if(sendto(sockfd,callback,strlen(callback),0,(sockaddr*)&cin,len)==-1){
+            perror("send error");
+            return -1;
+        }
+
+        if(strcmp(rbuf,"quit")==0)break;
+    }
+
+    //6.关闭网络通信
+    close(sockfd);
+    printf("close socket...\n");
+    return 0;
+}
